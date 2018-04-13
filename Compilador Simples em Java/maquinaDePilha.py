f5d8881e7196d714df91f1fd66c4f815f175f71e
Renata <rzjunges@inf.ufpel.edu.br>
2018-04-13 20:33:10 -0300

    Maquina de Pilha implementada em python e funcionando (testar com pilha_1 e pilha_2)

--- Compilador Simples em Java/maquinaDePilha.py
+++ Compilador Simples em Java/maquinaDePilha.py
@@ -1,27 +1,38 @@
 import sys 
 
 operacoes = ['SUB', 'SUM', 'DIV', 'MULT']
-stack = []
-teste = []
+stack = [] #inicializa a pilha
+
 with open(sys.argv[1], 'r') as arq: #Leitura do arquivo passado na linha de comando
     linha = arq.readline()
     
     #Le linha por linha até terminar o arquivo
     while(linha):
-        comando = linha.split(' ')[0]
+        comando = linha.split(' ')[0] #Separa o comando PUSH do seu numero
+        comando = comando.replace("\n", "") #remove o quebra linha se houver (demais comandos)
         
         if comando == 'PUSH':
-            numero = linha.split(' ')[1]
-            stack.append(int(numero))
+            numero = linha.split(' ')[1] 
+            stack.append(int(numero)) #adiciona numero na pilha
         
-       ''' else:
-            comando.split('\n')[0] #remove o quebra linha
-        teste.append(comando)
-        print(teste)
-        if comando == 'MULT':
-              print('aqui')
-        '''
-        print(stack)
+        if comando in operacoes:
+            arg1 = stack.pop() #Remove da pilha o primeiro numero
+            arg2 = stack.pop() #Remove da pilha o segundo numero
+
+            if comando == 'SUB':
+                stack.append(arg2 - arg1)
+            elif comando == 'SUM':
+                stack.append(arg1 + arg2) 
+            elif comando == 'MULT':
+                stack.append(arg1 * arg2)
+            elif comando == 'DIV':
+                stack.append(arg2 / arg1)
+        
+        print("Operação: {} \nO estado atual da pilha é: {}\n" .format(comando, stack)) #Exibe a pilha na tela 
+        
+        if comando == 'PRINT':
+            print("Resultado final da pilha: {}" .format(stack[0]));
+        
         linha = arq.readline()
 
 arq.close() #fecha arquivo
