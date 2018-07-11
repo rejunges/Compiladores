6c2265a3b567b9e73325588a6c164a236220d8f7
Renata <rzjunges@inf.ufpel.edu.br>
2018-07-11 16:48:37 -0300

    Printer printer funcionando

--- Lugosi - Tradução Dirigida por Sintaxe/script.sh
+++ Lugosi - Tradução Dirigida por Sintaxe/script.sh
@@ -2,4 +2,9 @@
 
 javacc Lugosi.jj
 javac *.java
-java Lugosi arq
+#java Lugosi arq
+#java Lugosi teste.lug
+#java Lugosi teste1.lug
+#java Lugosi teste2.lug
+#java Lugosi teste3.lug
+java Lugosi teste4.lug
