33e69a2fcef6b449adcf9e0c69a07498d71b88ec
Renata <rzjunges@inf.ufpel.edu.br>
2018-07-18 20:52:32 -0300

    Finalização do trabalho

--- Lugosi - Tradução Dirigida por Sintaxe/c-script.sh
+++ Lugosi - Tradução Dirigida por Sintaxe/c-script.sh
@@ -4,9 +4,12 @@ javacc Lugosi-c.jj
 javac *.java
 #java Lugosi arq
 #java Lugosi teste.lug
-java Lugosi teste1.lug > teste1.c
+java Lugosi teste1.lug 
 gcc teste1.c -o t
 ./t
 #java Lugosi teste2.lug
+#java Lugosi teste2.lug > teste2.c
+#gcc teste2.c -o t
+#./t
 #java Lugosi teste3.lug
 #java Lugosi teste4.lug
--- Lugosi - Tradução Dirigida por Sintaxe/script.sh
+++ Lugosi - Tradução Dirigida por Sintaxe/script.sh
@@ -5,6 +5,6 @@ javac *.java
 #java Lugosi arq
 #java Lugosi teste.lug
 #java Lugosi teste1.lug
-#java Lugosi teste2.lug
+java Lugosi teste2.lug
 #java Lugosi teste3.lug
-java Lugosi teste4.lug
+#java Lugosi teste4.lug
